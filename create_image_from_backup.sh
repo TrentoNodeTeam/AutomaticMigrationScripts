b7eaeb73ae19037b7fec2a754554aa9182c84f18
GenciTallabaciTN <genci.tallabaci.coll@trentinonetwork.it>
2016-06-21 10:43:18 +0200

    Update create_image_from_backup.sh

--- create_image_from_backup.sh
+++ create_image_from_backup.sh
@@ -10,6 +10,7 @@ echo "#!/bin/bash" > $TENANT_FILE
 # for every VM in the vm_file
 for vm in `cat $VM_FILE`;
 do
+
 TENANT_ID=`nova show $vm | grep tenant_id  | \
    igawk -F '|' '
    @include trims.awk
@@ -22,6 +23,5 @@ VM_NAME=`nova show $vm | grep '| name'  | \
 
 #echo $TENANT_ID $VM_NAME
 echo "glance image-create --name $VM_NAME --disk-format=qcow2 --container-format=bare --file snapshot-$vm.qcow  --is-public false --owner $TENANT_ID"  >> $TENANT_FILE "--progress"
-
 
 done
